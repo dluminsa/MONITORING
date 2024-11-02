fb884b5f47a18d346b9850fa51df1e7af7ecd3da
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-02 19:03:18 +0300

    Update MONITORING.py

--- MONITORING.py
+++ MONITORING.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from streamlit_gsheets import GSheetsConnection
 import streamlit as st
 import time
-df
 import datetime as dt
 st.write('BEING UPDATED FOR NOW')
 st.stop()
